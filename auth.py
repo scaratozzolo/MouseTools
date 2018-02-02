f14e10e491e2ef8ea2a9528cdabf2b5b534b07e2
Scott Caratozzolo <scaratozzolo12@gmail.com>
2018-02-01 20:33:08 -0500

    created getHeaders function, returns headers for request

--- auth.py
+++ auth.py
@@ -1,10 +1,30 @@
 import requests
 import json
+from datetime import datetime, timedelta
 
 
-
-#Gets an authentication(access) token from Disney
 def Authenticaton():
+    """Gets an authentication(access) token from Disney and returns it"""
+
     r = requests.get("https://disneyworld.disney.go.com/authentication/get-client-token")
     auth = json.loads(r.text)
-    return auth['access_token']
+    return auth['access_token'], auth['expires_in']
+
+
+time_of_expire = None
+access_token = None
+
+def getHeaders():
+    """Creates the headers to send during the request and returns it"""
+
+    global time_of_expire
+    global access_token
+
+    if time_of_expire == None or (datetime.now() > time_of_expire):
+        access_token, expires_in = Authenticaton()
+        time_of_expire = datetime.now() + timedelta(seconds=(expires_in-10))
+        headers = {"Authorization":"BEARER {}".format(access_token)}
+    else:
+        headers = {"Authorization":"BEARER {}".format(access_token)}
+
+    return headers
