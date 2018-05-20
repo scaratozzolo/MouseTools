495cf7bdff0625730f84cd3c84a35e3021098422
Scott Caratozzolo <scaratozzolo12@gmail.com>
2018-05-20 16:08:55 -0400

    Points of Interest class

---
+++ pointsofinterest.py
@@ -0,0 +1,100 @@
+import requests
+import json
+import sys
+from datetime import datetime, timedelta
+from auth import getHeaders
+
+class PointOfInterest(object):
+
+    def __init__(self, id = ''):
+        """
+        Constructor Function
+        Gets all points of interest data available and stores various elements into variables.
+        ID must be a string.
+        """
+
+        try:
+
+            if id == '':
+                raise ValueError
+            elif id != None and type(id) != str:
+                raise TypeError
+
+            self.__id = id
+
+            s = requests.get("https://api.wdpro.disney.go.com/facility-service/points-of-interest/{}".format(self.__id), headers=getHeaders())
+            self.__data = json.loads(s.content)
+
+            self.__point_of_interest_name = self.__data['name'].replace(u"\u2019", "'").replace(u"\u2013", "-").replace(u"\u2122", "").replace(u"\u2022", "-").replace(u"\u00ae", "").replace(u"\u2014", "-").replace(u"\u00a1", "").replace(u"\u00ee", "i").strip()
+            self.__type = self.__data['type']
+            self.__subType = self.__data['subType']
+
+        except ValueError:
+            print('PointOfInterest object expects an id value. Must be passed as string.\n Usage: Entertainment(id = None)')
+            sys.exit()
+        except TypeError:
+            print('PointOfInterest object expects a string argument.')
+            sys.exit()
+        except Exception:
+            print('That point of interest or ID is not available. {}'.format(id))
+            print('Full list of possible points of interest and their ID\'s can be found here: https://scaratozzolo.github.io/MouseTools/pointsofinterest.txt')
+            sys.exit()
+
+    def getPointOfInterestName(self):
+        """
+        Returns the name of the point of interest
+        """
+        return self.__point_of_interest_name
+
+    def getPointOfInterestID(self):
+        """
+        Returns the ID of the point of interest
+        """
+        return self.__id
+
+    def getAncestorDestination(self):
+        """
+        Returns the Ancestor Destination of the point of interest
+        """
+        try:
+            return self.__data['ancestorDestination']['links']['self']['title']
+        except:
+            return None
+    def getAncestorResortArea(self):
+        """
+        Returns the Ancestor Resort Area for the point of interest
+        """
+        try:
+            return self.__data['links']['ancestorResortArea']['title']
+        except:
+            return None
+
+    def getAncestorThemePark(self):
+        """
+        Returns the Ancestor Theme Park for the point of interest
+        """
+        try:
+            return self.__data['links']['ancestorThemePark']['title']
+        except:
+            return None
+    def getAncestorLand(self):
+        """
+        Returns the Ancestor Land for the point of interest
+        """
+        try:
+            return self.__data['links']['ancestorLand']['title']
+        except:
+            return None
+            
+    def __formatDate(self, month, day):
+        """
+        Formats month and day into proper format
+        """
+        if len(month) < 2:
+            month = '0'+month
+        if len(day) < 2:
+            day = '0'+day
+        return month, day
+
+    def __str__(self):
+        return 'PointOfInterest object for {}'.format(self.__point_of_interest_name)
