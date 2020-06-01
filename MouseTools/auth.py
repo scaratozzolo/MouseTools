79de32dc3a4f56e9cd535f04eb211747a9d383ab
Scott Caratozzolo <scaratozzolo12@gmail.com>
2020-06-01 16:42:16 -0400

    couchbaseHeaders()

--- MouseTools/auth.py
+++ MouseTools/auth.py
@@ -23,8 +23,13 @@ def getHeaders():
     if time_of_expire == None or (datetime.now() > time_of_expire):
         access_token, expires_in = Authentication()
         time_of_expire = datetime.now() + timedelta(seconds=(expires_in-10))
-        headers = {"Authorization":"BEARER {}".format(access_token)}
+        headers = {"authorization":"BEARER {}".format(access_token), "grant_type":"assertion", "assertion_type":"public", "client_id":"WDPRO-MOBILE.MDX.WDW.ANDROID-PROD"}
     else:
-        headers = {"Authorization":"BEARER {}".format(access_token)}
+        headers = {"authorization":"BEARER {}".format(access_token)}
 
     return headers
+
+def couchbaseHeaders():
+
+    header = {"Authorization":"Basic RFBFQ1AtTU9CSUxFLldEVy5BTkRST0lELVBST0Q6RGhyeHMyZHVReGdiVjZ5Mg==","User-Agent":"CouchbaseLite/1.3 (1.4.1/8a21c5927a273a038fb3b66ec29c86425e871b11)","Content-Type":"application/json","Accept":"multipart/related"}
+    return header
