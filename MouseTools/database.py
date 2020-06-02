1177839a33460b3195ee314ac1cca7e9549a0f7a
Scott Caratozzolo <scaratozzolo12@gmail.com>
2020-06-02 16:59:41 -0400

    actual fix for ids with 2 ids and types

--- MouseTools/database.py
+++ MouseTools/database.py
@@ -1,4 +1,5 @@
 from .auth import couchbaseHeaders
+import pkg_resources
 import sqlite3
 import requests
 import json
@@ -9,8 +10,8 @@ import re
 class DisneyDatabase:
 
     def __init__(self):
-        self.db_name = "MouseTools.db"
-        conn = sqlite3.connect(self.db_name)
+        self.db_path = pkg_resources.resource_filename("MouseTools", "MouseTools.db")
+        conn = sqlite3.connect(self.db_path)
         c = conn.cursor()
 
         self.create_last_sequence_table()
@@ -29,7 +30,7 @@ class DisneyDatabase:
 
     def create_last_sequence_table(self):
 
-        conn = sqlite3.connect(self.db_name)
+        conn = sqlite3.connect(self.db_path)
         c = conn.cursor()
 
         c.execute("""CREATE TABLE IF NOT EXISTS lastSequence (channel TEXT PRIMARY KEY, value TEXT)""")
@@ -40,9 +41,9 @@ class DisneyDatabase:
 
     def create_facilities_table(self):
 
-        conn = sqlite3.connect(self.db_name)
+        conn = sqlite3.connect(self.db_path)
         c = conn.cursor()
-
+        # subType
         c.execute("""CREATE TABLE IF NOT EXISTS facilities (id TEXT PRIMARY KEY, name TEXT, entityType TEXt, couchbase_id TEXT, destination_code TEXT, park_id TEXT, resort_id TEXT, land_id TEXT, resort_area_id TEXT, entertainment_venue_id TEXT)""")
 
         conn.commit()
@@ -51,7 +52,7 @@ class DisneyDatabase:
 
     def create_sync_table(self):
 
-        conn = sqlite3.connect(self.db_name)
+        conn = sqlite3.connect(self.db_path)
         c = conn.cursor()
 
         c.execute("""CREATE TABLE IF NOT EXISTS sync (id TEXT PRIMARY KEY, rev TEXT, body TEXT, channel TEXT)""")
@@ -62,7 +63,7 @@ class DisneyDatabase:
 
     def channel_exists(self, channel):
 
-        conn = sqlite3.connect(self.db_name)
+        conn = sqlite3.connect(self.db_path)
         c = conn.cursor()
 
         facility_channel_exists = c.execute("""SELECT COUNT(value) FROM lastSequence WHERE channel = '{}'""".format(channel)).fetchone()[0]
@@ -71,27 +72,8 @@ class DisneyDatabase:
         conn.commit()
         conn.close()
 
-    def __additional_id(self, id):
-
-        payload = {"docs" : [{
-            "id": id
-          }], 'json':True}
-        r = requests.post("https://realtime-sync-gw.wdprapps.disney.com/park-platform-pub/_bulk_get?revs=true&attachments=true", data=json.dumps(payload), headers=couchbaseHeaders())
-        s = r.text
-
-        cont_reg = re.compile("\w+-\w+:\s\w+\/\w+")
-        s = re.sub(cont_reg, "", s)
-        s = s.splitlines()
-
-        return s
-
     def create_channel(self, channel):
-        # wdw.facilities.1_0.en_us
-        # dlr.facilities.1_0.en_us
-        # wdw.facilitystatus.1_0
-        # dlr.facilitystatus.1_0
-        # wdw.calendar.1_0
-        # dlr.calendar.1_0
+        # no transportation entities
         payload = {
             "channels": channel,
             "style": 'all_docs',
@@ -102,11 +84,12 @@ class DisneyDatabase:
         r = requests.post("https://realtime-sync-gw.wdprapps.disney.com/park-platform-pub/_changes?feed=normal&heartbeat=30000&style=all_docs&filter=sync_gateway%2Fbychannel", data=json.dumps(payload), headers=couchbaseHeaders())
         s = json.loads(r.text)
 
-        conn = sqlite3.connect(self.db_name)
+        conn = sqlite3.connect(self.db_path)
         c = conn.cursor()
 
         c.execute("""INSERT INTO lastSequence (channel, value) VALUES ('{}', '{}')""".format(channel, s['last_seq']))
 
+        # search for deleted: i['deleted'] or i['removed']
         docs = []
         for i in s['results']:
             this = {}
@@ -114,6 +97,11 @@ class DisneyDatabase:
 
             docs.append(this)
 
+            split_id = i['id'].split(":")
+            if len(split_id) > 1:
+                this = {}
+                this['id'] = split_id[0]
+                docs.append(this)
 
         # print('getting {} docs'.format(len(docs)))
 
@@ -125,15 +113,15 @@ class DisneyDatabase:
         s = re.sub(cont_reg, "", s)
         s = s.splitlines()
         for x in s:
-            if x != '':
+            if x != '' and x[0] != '-':
                 try:
                     x = json.loads(x)
-                    c.execute("""INSERT INTO sync (id, rev, body, channel) VALUES ('{}', '{}', '{}', '{}')""".format(x['_id'], x['_rev'], json.dumps(x), channel))
+                    query = """INSERT INTO sync (id, rev, body, channel) VALUES ('{}', '{}', '{}', '{}')""".format(x['_id'], x['_rev'], json.dumps(x).replace("'", "''"), channel)
+                    c.execute(query)
 
                     split_id = x['_id'].split(':')
                     this['id'] = split_id[-1].split(';')[0].split('.')[-1]
 
-                    s.extend(self.__additional_id(split_id[0]))
 
                     this['name'] = x['name']
                     this['entityType'] = x['type']
@@ -143,7 +131,10 @@ class DisneyDatabase:
                     try:
                         this['park_id'] = x['ancestorThemeParkId'].split(';')[0]
                     except:
-                        this['park_id'] = '0'
+                        try:
+                            this['park_id'] = x['ancestorWaterParkId'].split(';')[0]
+                        except:
+                            this['park_id'] = '0'
 
                     try:
                         this['land_id'] = x['ancestorLandId'].split(';')[0]
@@ -165,8 +156,11 @@ class DisneyDatabase:
                     except:
                         this['ev_id'] = '0'
 
-                    c.execute("""INSERT INTO facilities (id, name, entityType, couchbase_id, destination_code, park_id, land_id, resort_id, resort_area_id, entertainment_venue_id) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')""".format(this['id'], this['name'], this['entityType'], this['cb_id'], this['dest_code'], this['park_id'], this['land_id'], this['resort_id'], this['ra_id'], this['ev_id']))
+                    query = """INSERT INTO facilities (id, name, entityType, couchbase_id, destination_code, park_id, land_id, resort_id, resort_area_id, entertainment_venue_id) VALUES ("{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}")""".format(this['id'], this['name'].replace("'", "''"), this['entityType'], this['cb_id'], this['dest_code'], this['park_id'], this['land_id'], this['resort_id'], this['ra_id'], this['ev_id'])
+                    # print(query)
+                    c.execute(query)
                 except Exception as e:
+                    # print(x)
                     # print(e)
                     continue
 
@@ -177,7 +171,7 @@ class DisneyDatabase:
 
     def sync_database(self):
 
-        conn = sqlite3.connect(self.db_name)
+        conn = sqlite3.connect(self.db_path)
         c = conn.cursor()
 
         for row in c.execute("""SELECT * FROM lastSequence""").fetchall():
@@ -191,17 +185,24 @@ class DisneyDatabase:
             r = requests.post("https://realtime-sync-gw.wdprapps.disney.com/park-platform-pub/_changes?feed=normal&heartbeat=30000&style=all_docs&since={}&filter=sync_gateway%2Fbychannel".format(row[1]), data=json.dumps(payload), headers=couchbaseHeaders())
             s = json.loads(r.text)
 
-            conn = sqlite3.connect(self.db_name)
+            conn = sqlite3.connect(self.db_path)
             c = conn.cursor()
 
-            c.execute("""INSERT OR REPLACE INTO lastSequence (channel, value) VALUES ('{}', '{}')""".format(row[0], s['last_seq']))
+            c.execute("""REPLACE INTO lastSequence (channel, value) VALUES ('{}', '{}')""".format(row[0], s['last_seq']))
 
+            # search for deleted: i['deleted'] or i['removed']
             docs = []
             for i in s['results']:
                 this = {}
                 this['id'] = i['id']
 
                 docs.append(this)
+
+                split_id = i['id'].split(":")
+                if len(split_id) > 1:
+                    this = {}
+                    this['id'] = split_id[0]
+                    docs.append(this)
 
             # print('updating {} docs'.format(len(docs)))
 
@@ -216,12 +217,12 @@ class DisneyDatabase:
                 if x != '':
                     try:
                         x = json.loads(x)
-                        c.execute("""INSERT OR INTO sync (id, rev, body, channel) VALUES ('{}', '{}', '{}', '{}')""".format(x['_id'], x['_rev'], json.dumps(x), channel))
+                        query = """INSERT OR REPLACE INTO sync (id, rev, body, channel) VALUES ('{}', '{}', '{}', '{}')""".format(x['_id'], x['_rev'], json.dumps(x).replace("'", "''"), channel)
+                        c.execute(query)
 
                         split_id = x['_id'].split(':')
                         this['id'] = split_id[-1].split(';')[0].split('.')[-1]
 
-                        s.extend(self.__additional_id(split_id[0]))
 
                         this['name'] = x['name']
                         this['entityType'] = x['type']
@@ -253,7 +254,9 @@ class DisneyDatabase:
                         except:
                             this['ev_id'] = '0'
 
-                        c.execute("""INSERT OR INTO facilities (id, name, entityType, couchbase_id, destination_code, park_id, land_id, resort_id, resort_area_id, entertainment_venue_id) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')""".format(this['id'], this['name'], this['entityType'], this['cb_id'], this['dest_code'], this['park_id'], this['land_id'], this['resort_id'], this['ra_id'], this['ev_id']))
+                        query = """INSERT OR REPLACE INTO facilities (id, name, entityType, couchbase_id, destination_code, park_id, land_id, resort_id, resort_area_id, entertainment_venue_id) VALUES ("{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}")""".format(this['id'], this['name'].replace("'", "''"), this['entityType'], this['cb_id'], this['dest_code'], this['park_id'], this['land_id'], this['resort_id'], this['ra_id'], this['ev_id'])
+                        # print(query)
+                        c.execute(query)
                     except Exception as e:
                         # print(e)
                         continue
