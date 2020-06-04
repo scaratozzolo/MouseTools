7e89011247ff16446bb53aa4e8ef072396ee99bc
Scott Caratozzolo <scaratozzolo12@gmail.com>
2020-06-04 00:28:57 -0400

    rewrite

--- MouseTools/facilities.py
+++ MouseTools/facilities.py
@@ -1,111 +1,184 @@
 import requests
 import json
 import sys
+import sqlite3
 from datetime import datetime, timedelta
 from .auth import getHeaders
+from .database import DisneyDatabase
 
 
 class Facility(object):
 
-    def __init__(self, id = ''):
+    def __init__(self, id = '', sync_on_init=True):
         """
         Constructor Function
         Gets all facility data available and stores various elements into variables.
         ID must be a string
         """
-
+        # TODO Maybe turn this into a base class
         try:
 
-            if id == '':
-                raise ValueError
-            elif id != None and type(id) != str:
-                raise TypeError
+            self.__db = DisneyDatabase(sync_on_init)
+            conn = sqlite3.connect(self.__db.db_path)
+            c = conn.cursor()
 
-            self.__id = id
+            row = c.execute("SELECT * FROM facilities WHERE id = ?", (id,)).fetchone()
+            if row is None:
+                raise ValueError()
+            else:
+                self.__id = row[0]
+                self.__name = row[1]
+                self.__entityType = row[2]
+                self.__subType = row[3]
+                self.__doc_id = row[4]
+                self.__dest_code = row[5]
+                self.__anc_park_id = row[6]
+                self.__anc_resort_id = row[7]
+                self.__anc_land_id = row[8]
+                self.__anc_ra_id = row[9]
+                self.__anc_ev_id = row[10]
 
-            s = requests.get("https://api.wdpro.disney.go.com/facility-service/facilities/{}".format(self.__id), headers=getHeaders())
-            self.__data = json.loads(s.content)
-
-            self.__facility_name = self.__data['name'].replace(u"\u2019", "'").replace(u"\u2013", "-").replace(u"\u2122", "").replace(u"\u2022", "-").replace(u"\u00ae", "").replace(u"\u2014", "-").replace(u"\u00a1", "").replace(u"\u00ee", "i").replace(u"\u25cf", " ").replace(u"\u00e9", "e").replace(u"\u00ad", "").replace(u"\u00a0", " ").replace(u"\u00e8", "e").replace(u"\u00eb", "e").replace(u"\u2026", "...").replace(u"\u00e4", "a").replace(u"\u2018", "'").replace(u"\u00ed", "i").replace(u"\u201c", '"').replace(u"\u201d", '"').strip()
-
-            try:
-                self.__subType = self.__data['subType']
-            except:
-                self.__subType = None
-
-        except ValueError:
-            print('Facility object expects an id value. Must be passed as string.\n Usage: Facility(id)')
-            sys.exit()
-        except TypeError:
-            print('Facility object expects a string argument.')
-            sys.exit()
-        except Exception:
-            print('That facility or ID is not available.')
-            print('Full list of possible facilities and their ID\'s can be found here: https://scaratozzolo.github.io/MouseTools/facility.txt')
+            self.__facilities_data = c.execute("SELECT body FROM sync WHERE id = ?", (self.__doc_id,)).fetchone()[0]
+        except Exception as e:
+            print(e)
+            print('That park is not available.')
             sys.exit()
 
+    def get_possible_ids(self):
+        """Returns a list of possible ids of this entityType"""
+        conn = sqlite3.connect(DisneyDatabase().db_path)
+        c = conn.cursor()
+        pos_ids = [row[0] for row in c.execute("SELECT id FROM facilities WHERE entityType ?", (self.__entityType,))]
+        return pos_ids
 
-    def getFacilityName(self):
-        """
-        Returns the name of the facility
-        """
-        return self.__facility_name
-
-    def getFacilityID(self):
-        """
-        Returns the id of the facility
-        """
+    def get_id(self):
+        """Return object id"""
         return self.__id
 
-    def getFacilitySubType(self):
-        """
-        Returns the facility sub type.
-        """
+    def get_name(self):
+        """Return object name"""
+        return self.__name
+
+    def get_entityType(self):
+        """Return object entityType"""
+        return self.__entityType
+
+    def get_subType(self):
+        """Return object subType"""
         return self.__subType
 
-    def getAncestorResortArea(self):
-        """
-        Returns ancestor resort area
-        """
-        try:
-            return self.__data['links']['ancestorResortArea']['title']
-        except:
-            return None
+    def get_doc_id(self):
+        """Return object doc id"""
+        return self.__doc_id
 
-    def getAncestorThemePark(self):
-        """
-        Returns ancestor theme park
-        """
-        try:
-            return self.__data['links']['ancestorThemePark']['title']
-        except:
-            return None
+    def get_destination_code(self):
+        """Return object destination code"""
+        return self.__dest_code
 
-    def getAncestorLand(self):
-        """
-        Returns ancestor land
-        """
-        try:
-            return self.__data['links']['ancestorLand']['title']
-        except:
-            return None
+    def get_ancestor_park_id(self):
+        """Return object ancestor theme or water park id"""
+        return self.__anc_park_id
 
-    def getAncestorDestination(self):
-        """
-        Returns ancestor destination
-        """
-        try:
-            return self.__data['links']['ancestorDestination']['title']
-        except:
-            return None
+    def get_ancestor_resort_id(self):
+        """Return object ancestor resort id"""
+        return self.__anc_resort_id
 
-    def __formatDate(self, num):
+    def get_ancestor_land_id(self):
+        """Return object land id"""
+        return self.__anc_land_id
+
+    def get_ancestor_resort_area_id(self):
+        """Return object resort area id"""
+        return self.__anc_ra_id
+
+    def get_ancestor_entertainment_venue_id(self):
+        """Return object entertainment venue id"""
+        return self.__anc_ev_id
+
+    def get_raw_facilities_data(self):
+        """Returns the raw facilities data currently stored in the database"""
+        return self.__facilities_data
+
+    def get_status(self):
+        """Return current status of the object."""
+        if self.__db.channel_exists('{}.today.1_0'.format(self.__dest_code)):
+            self.__db.sync_today_channel()
+            # maybe just sync this channel? and do same for previous methods
+        else:
+            self.__db.create_today_channel('{}.today.1_0'.format(self.__dest_code))
+
+        conn = sqlite3.connect(self.__db.db_path)
+        c = conn.cursor()
+
+        today_data = c.execute("SELECT body FROM sync WHERE id = ?", ('{}.today.1_0.{}'.format(self.__dest_code, self.__entityType),)).fetchone()
+
+        if today_data is None:
+            return None
+        else:
+            body = json.loads(today_data[0])
+
+            return body['facilities'][self.__id + ';entityType=' + self.__entityType][0]['scheduleType']
+
+    def get_last_update(self):
+        """Returns facilities last update time as a datetime object"""
+        facility_data = json.loads(self.__facilities_data)
+        return datetime.strptime(facility_data['lastUpdate'], "%Y-%m-%dT%H:%M:%SZ")
+        # TODO check if facilitystatus has a different last update time
+
+    def get_coordinates(self):
+        """Returns the object's latitude and longitude"""
+        facility_data = json.loads(self.__facilities_data)
+        return facility_data['latitude'], facility_data['longitude']
+
+    def get_description(self):
+        """Returns the object's descriptions"""
+        facility_data = json.loads(self.__facilities_data)
+        return facility_data['description']
+
+    def get_list_image(self):
+        """Returns the url to the object's list image"""
+        facility_data = json.loads(self.__facilities_data)
+        return facility_data['listImageUrl']
+
+    def get_facets(self):
+        """Returns a list of  dictionaries of the object's facets"""
+        facility_data = json.loads(self.__facilities_data)
+        return facility_data['facets']
+
+    def get_todays_hours(self):
+        """Returns the start and end times for the object. Will return None, None if closed"""
+        start_time = None
+        end_time = None
+
+        if self.__db.channel_exists('{}.today.1_0'.format(self.__dest_code)):
+            self.__db.sync_today_channel()
+            # maybe just sync this channel? and do same for previous methods
+        else:
+            self.__db.create_today_channel('{}.today.1_0'.format(self.__dest_code))
+
+        conn = sqlite3.connect(self.__db.db_path)
+        c = conn.cursor()
+
+        today_data = c.execute("SELECT body FROM sync WHERE id = ?", ("{}.today.1_0.{}".format(self.__dest_code, self.__entityType),)).fetchone()
+
+        if today_data is None:
+            return start_time, end_time
+        else:
+            body = json.loads(today_data[0])
+
+            if body['facilities'][self.__id + ';entityType=' + self.__entityType][0]['scheduleType'] == 'Closed' or body['facilities'][self.__id + ';entityType=Attraction'][0]['scheduleType'] == 'Refurbishment':
+                return start_time, end_time
+
+            start_time = datetime.strptime(body['facilities'][self.__id + ';entityType=' + self.__entityType][0]['startTime'], "%Y-%m-%dT%H:%M:%SZ")
+            end_time = datetime.strptime(body['facilities'][self.__id + ';entityType=' + self.__entityType][0]['endTime'], "%Y-%m-%dT%H:%M:%SZ")
+
+            return start_time, end_time
+
+    def __eq__(self, other):
         """
-        Formats month and day into proper format
+        Checks if objects are equal
         """
-        if len(num) < 2:
-            num = '0'+num
-        return num
+        return self.__id == other.get_id()
 
     def __str__(self):
-        return 'Facility object for {}'.format(self.__facility_name)
+        return 'Facility object for {}'.format(self.__name)
