fe1346d9de4eb2c6db48419692f5c18265a14d8a
Scott Caratozzolo <scaratozzolo12@gmail.com>
2020-08-18 21:37:09 -0400

    2.0.2 closes #6

--- MouseTools/__init__.py
+++ MouseTools/__init__.py
@@ -12,6 +12,6 @@ import MouseTools.ids
 
 name = "MouseTools"
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 __all__ = ["Destination", "Park", "EntertainmentVenue", "Attraction", "Entertainment", "Facility", "Character", "PointOfInterest", "DisneyDatabase"]
