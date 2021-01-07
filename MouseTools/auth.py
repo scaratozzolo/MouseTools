5586eae0c692372d36faeb7ec6e2cd8e5e393135
Scott Caratozzolo <scaratozzolo12@gmail.com>
2021-01-07 16:37:24 -0500

    fixed headers for general disney api requests

--- MouseTools/auth.py
+++ MouseTools/auth.py
@@ -4,10 +4,10 @@ from datetime import datetime, timedelta
 
 
 def Authentication():
-    """Gets an authentication(access) token from Disney and returns it"""
+    """Gets an authentication (access) token from Disney and returns it"""
 
-    r = requests.get("https://disneyworld.disney.go.com/authentication/get-client-token")
-    auth = json.loads(r.content)
+    headers = {"User-Agent":"CouchbaseLite/1.3 (1.4.1/8a21c5927a273a038fb3b66ec29c86425e871b11)","Content-Type":"application/json","Accept":"multipart/related"}
+    auth = requests.get("https://disneyworld.disney.go.com/authentication/get-client-token", headers=headers).json()
     return auth['access_token'], auth['expires_in']
 
 
@@ -23,9 +23,9 @@ def getHeaders():
     if time_of_expire == None or (datetime.now() > time_of_expire):
         access_token, expires_in = Authentication()
         time_of_expire = datetime.now() + timedelta(seconds=(expires_in-10))
-        headers = {"Authorization":"BEARER {}".format(access_token)}
+        headers = {"Authorization":"BEARER {}".format(access_token), "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36", "Content-Type":"application/json;charset=UTF-8","Accept":"*/*"}
     else:
-        headers = {"Authorization":"BEARER {}".format(access_token)}
+        headers = {"Authorization":"BEARER {}".format(access_token), "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36", "Content-Type":"application/json;charset=UTF-8","Accept":"*/*"}
 
     return headers
 
